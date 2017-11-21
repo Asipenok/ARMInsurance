b7876178b8321f3924025a134bb46fd888c9f310
Denis Asipenok <asipenok@gmail.com>
2017-11-21 13:02:16 +0300

    addPerson
    data person added

--- src/inshurer/model/BaseData.java
+++ src/inshurer/model/BaseData.java
@@ -13,7 +13,7 @@ public class BaseData {
     private static final String URL = "jdbc:mysql://localhost:3306/insurance";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
-    private static final String INSERT_PERSON = "INSERT INTO person VALUES (?,?,?,?,?)";
+    private static final String INSERT_PERSON = "INSERT INTO person (FirstName, LastName, MiddleName, PersonalNumber) VALUES (?,?,?,?)";
     private Connection connection;
     private PreparedStatement preparedStatement;
 
@@ -41,14 +41,30 @@ public class BaseData {
 
         preparedStatement = connection.prepareStatement(INSERT_PERSON);
         try {
-
-            preparedStatement.setString(2, first_name);
-            preparedStatement.setString(3, last_name);
-            preparedStatement.setString(4, middle_name);
+            preparedStatement.setString(1, first_name);
+            preparedStatement.setString(2, last_name);
+            preparedStatement.setString(3, middle_name);
             preparedStatement.setString(4, personal_number);
-
             preparedStatement.execute();
             connection.close();
+            System.out.println("person add");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    //метод вставки клиента в БД
+    public void findPerson(String first_name, String last_name, String middle_name, String personal_number) throws SQLException {
+
+        preparedStatement = connection.prepareStatement(INSERT_PERSON);
+        try {
+            preparedStatement.setString(1, first_name);
+            preparedStatement.setString(2, last_name);
+            preparedStatement.setString(3, middle_name);
+            preparedStatement.setString(4, personal_number);
+            preparedStatement.execute();
+            connection.close();
+            System.out.println("person add");
         } catch (SQLException e) {
             e.printStackTrace();
         }
